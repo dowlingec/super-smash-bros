80ecb4905011fa54ee072f7e258b5a4d91bfa9aa
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-05 12:23:07 -0400

    added transform

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import Fighter from './components/Fighter';
 
 function App() {
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
-
+        <Fighter  name={'Roy'} color={'#fff'}/>
+        <Fighter name={'Fox'} color={'green'}/>
+        <Fighter name={'Ganondorf'} color={'purple'}/>
       </div>
     </div>
   );
 }
 
 export default App;
+
+// how this would look in vanilla js
+//const Fighter = {name: 'Roy', color: 'red'}
+// each props is a key: 'value' pair
