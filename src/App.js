c1c220f4319cf3c0ce407fa9f1665d66dd1a1f8d
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-05 13:52:22 -0400

    onclick visible

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Fighter from './components/Fighter';
 import FighterScreen from './components/FighterScreen';
+import {useState} from 'react';
 
 function App() {
+  const [visible, setVisible] = useState(false)
   return (
     <div className="App">
       <h1>Fighters</h1>
       <div className="fighters-grid">
-        <Fighter  name={'Roy'} color={'#fff'} />
-        <Fighter name={'Fox'} color={'green'} />
+        <Fighter  name={'Roy'} color={'#fff'} setVisible={setVisible}/>
+        <Fighter name={'Fox'} color={'green'} setVisible={setVisible} />
         <Fighter name={'Ganondorf'} color={'purple'} />
         <Fighter name={'Sheik'} color={'lightblue'} />
       </div>
-      <FighterScreen />
+      <FighterScreen isVisible={visible} setVisible={setVisible}/>
     </div>
   );
 }
--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,11 +1,12 @@
-const FighterScreen = () => {
-    <div className="fighter-screen">
+const FighterScreen = ({isVisible, setVisible}) => {
+    return (
+    <div onClick={() => {setVisible(false)}}className="fighter-screen" style={{display: isVisible ? "block" : "none"}}>
         <h2>Roy</h2>
         <img width="100" height="100" src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, modi nobis. Vero harum ratione nostrum suscipit doloribus dolorum, vel asperiores non quidem, quibusdam atque laudantium culpa unde labore velit. Similique.
         </p>
-      </div>
+      </div>)
 }
 
 export default FighterScreen;
