2e1834c14bff2fa4076aa46266640e6ef718d500
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-05 17:52:31 -0400

    fighterScreen auto updating

--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,8 +1,8 @@
-const FighterScreen = ({isVisible, setVisible}) => {
+const FighterScreen = ({isVisible, setVisible, selectedCharacter}) => {
     return (
     <div onClick={() => {setVisible(false)}}className="fighter-screen" style={{display: isVisible ? "block" : "none"}}>
-        <h2>Roy</h2>
-        <img width="100" height="100" src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+        <h2>{selectedCharacter.name}</h2>
+        <img width="100" height="100" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectedCharacter.name.toLowerCase()}/main.png`} />
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, modi nobis. Vero harum ratione nostrum suscipit doloribus dolorum, vel asperiores non quidem, quibusdam atque laudantium culpa unde labore velit. Similique.
         </p>
