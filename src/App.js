d0a841404caf1c2220bfff78791ed03c08648e0f
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-05 13:14:59 -0400

    created fighter screen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
         <Fighter  name={'Roy'} color={'#fff'} />
         <Fighter name={'Fox'} color={'green'} />
         <Fighter name={'Ganondorf'} color={'purple'} />
+        <Fighter name={'Sheik'} color={'lightblue'} />
+      </div>
+      <div classNAme="fighter-screen">
+        <h2>Roy</h2>
+        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, modi nobis. Vero harum ratione nostrum suscipit doloribus dolorum, vel asperiores non quidem, quibusdam atque laudantium culpa unde labore velit. Similique.
+        </p>
       </div>
     </div>
   );
