e8a13aa9eaa43801b2bfcc1d6b96bbf62d15a910
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-05 13:25:38 -0400

    created fighter screen components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Fighter from './components/Fighter';
+import FighterScreen from './components/FighterScreen';
 
 function App() {
   return (
@@ -11,13 +12,7 @@ function App() {
         <Fighter name={'Ganondorf'} color={'purple'} />
         <Fighter name={'Sheik'} color={'lightblue'} />
       </div>
-      <div classNAme="fighter-screen">
-        <h2>Roy</h2>
-        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, modi nobis. Vero harum ratione nostrum suscipit doloribus dolorum, vel asperiores non quidem, quibusdam atque laudantium culpa unde labore velit. Similique.
-        </p>
-      </div>
+      <FighterScreen />
     </div>
   );
 }
---
+++ src/components/FighterScreen.js
@@ -0,0 +1,11 @@
+const FighterScreen = () => {
+    <div className="fighter-screen">
+        <h2>Roy</h2>
+        <img width="100" height="100" src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, modi nobis. Vero harum ratione nostrum suscipit doloribus dolorum, vel asperiores non quidem, quibusdam atque laudantium culpa unde labore velit. Similique.
+        </p>
+      </div>
+}
+
+export default FighterScreen;
